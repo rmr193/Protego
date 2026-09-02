db6ab5426443aa635c5498476ec75d5b7b2edb95
rmr193 <rajeshmozumder334@gmail.com>
2026-09-02 19:52:53 +0600

    chore: restructure monorepo build scripts, add backend gitignore, and update Vercel configuration

--- backend/dist/core/prisma.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const client_1 = require("@prisma/client");
-const logger_1 = require("../shared/utils/logger");
-const prisma = new client_1.PrismaClient();
-logger_1.logger.info('Initialized real Prisma Client connected to Supabase.');
-exports.default = prisma;
